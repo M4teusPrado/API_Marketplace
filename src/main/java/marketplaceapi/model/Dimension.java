5328d8f7d1d78d53d190907fbb4835e1f468368d
M4teusPrado <mateus.silvaprado@gmail.com>
2022-01-05 23:07:40 -0300

    Iniciando relacimento de classes

--- src/main/java/marketplaceapi/model/Dimension.java
+++ src/main/java/marketplaceapi/model/Dimension.java
@@ -2,12 +2,14 @@ package marketplaceapi.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
 
+@Entity
 @Data
 public class Dimension implements Serializable {
 
--- src/main/java/marketplaceapi/model/Price.java
+++ src/main/java/marketplaceapi/model/Price.java
@@ -1,12 +1,14 @@
 package marketplaceapi.model;
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
 
+@Entity
 @Data
 public class Price implements Serializable {
 
