3312614985a90f215676a776b6f4742c7651ea09
M4teusPrado <mateus.silvaprado@gmail.com>
2022-01-12 23:45:42 -0300

    Commit diario

--- src/main/java/marketplaceapi/controllers/ItemController.java
+++ src/main/java/marketplaceapi/controllers/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,5 @@ public class ItemController {
     public ResponseEntity<Item> getItensById(@PathVariable Long id ){
         return ResponseEntity.ok(itemService.getItemById(id));
     }
-
+//
 }
